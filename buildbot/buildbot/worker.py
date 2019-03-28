ec2fa55707b2956b71a154326cdaa1e4adf1a50f
Sachille Atapattu <sa2257@cornell.edu>
2019-03-28 11:09:21 -0400

    Add space between configs

--- buildbot/buildbot/worker.py
+++ buildbot/buildbot/worker.py
@@ -273,7 +273,7 @@ def _sds_cmd(task, config):
     poll  = '1'
 
     if task['config'].get('verbose'):
-        flags = flags + '-verbose'
+        flags = flags + ' ' + '-verbose'
     
     if task['config'].get('clock'):
         clock = task['config'].get('clock')
@@ -282,8 +282,8 @@ def _sds_cmd(task, config):
         poll = task['config'].get('poll-mode')
     
     if task['config'].get('time'):
-        flags = flags + '-instrument-stub'
-    
+        flags = flags + ' ' + '-instrument-stub'
+    # multiple flags are not as intended, they endup as '-f1 -f2' than -f1 -f2, but it still works 
     return prefix + [
         'sds++',
         '-sds-pf', SDS_PLATFORM,
